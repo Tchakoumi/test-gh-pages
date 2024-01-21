95e538fdde43927b005a4edc1c96f18920b78094
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-01-21 22:06:03 +0100

    refactor(app): #3 made myCars component a functional component

--- src/components/Mycars.js
+++ src/components/Mycars.js
@@ -1,103 +1,93 @@
-import { Component } from "react";
+import { useState } from "react";
 import Car from "./Cars";
 
-class Mycars extends Component {
-  state = {
+export default function Mycars() {
+  const [state, setState] = useState({
     voitures: [
       { name: "Ford", color: "red", year: 2000 },
       { name: "Mercedes", color: "black", year: 2010 },
+      { name: "Mercedes", color: "blue", year: 2020 },
       { name: "peugeat", color: "green", year: 2018 },
     ],
     titre: "Mon Catalogue Voitures2",
-    valeurInput:'',
-    name: '',
-      age: '',
-      color: '',
-  };
+    valeurInput: "",
+    name: "",
+    age: "",
+    color: "",
+  });
 
-  addTenYears = () => {
-    // console.log('bien')
-
-    const updateSate = this.state.voitures.map((param) => {
-      return (param.year -= 10);
+  const addTenYears = () => {
+    const updateSate = state.voitures.map((param) => {
+      return { ...param, year: param.year - 10 };
     });
-    this.setState({
+    setState({
       updateSate,
     });
   };
 
-  
-  
-  
-  handleChange = (e)=>{
-    this.setState({valeurInput:e.target.value});
-    // console.log(e.target.value)
-    console.log(this.state.valeurInput);
-  }
-  DeleteCar = ()=>{
-
-    console.log(this.state.newtable) ;
-    const newtable = this.state.voitures.slice(1);
-    this.setState({
-      voitures:newtable
-    })
-  }
-  DeleteSpecificCar=()=>{
-    const newTable = this.state.voitures.splice(this.state.valeurInput, 1)
-    console.log(newTable)
-    this.setState({
-      voiture:newTable
-    })
-
-  }
-
-
-  handleChangeForm = (e) => {
-    // Mettez à jour l'état avec les valeurs des champs de formulaire
-    this.setState({ [e.target.name]: e.target.value });
+  const handleChange = (e) => {
+    setState({ valeurInput: e.target.value });
+    console.log(state.valeurInput);
+  };
+  const DeleteCar = () => {
+    console.log(state.newtable);
+    const newtable = state.voitures.slice(1);
+    setState({
+      voitures: newtable,
+    });
+  };
+  const DeleteSpecificCar = () => {
+    const newTable = state.voitures.splice(state.valeurInput, 1);
+    console.log(newTable);
+    setState({
+      voiture: newTable,
+    });
   };
 
-  handleSubmit = (e) => {
+  const handleChangeForm = (e) => {
+    // Mettez à jour l'état avec les valeurs des champs de formulaire
+    setState({ [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     const nouvelleVoiture = {
-      name: this.state.name,
-      color: this.state.color,
-      year:this.state.year,
+      name: state.name,
+      color: state.color,
+      year: state.year,
       // year: new Date().getFullYear(), // Supposons que l'année est l'année actuelle
     };
 
-    this.setState((prevState) => ({
+    setState((prevState) => ({
       voitures: [...prevState.voitures, nouvelleVoiture],
-      name: '',
-      age: '',
-      color: '',
+      name: "",
+      age: "",
+      color: "",
     }));
 
     // Affichez les valeurs des champs dans la console (remplacez par votre logique d'enregistrement)
-    // console.log('name:', this.state.name);
-    // console.log('year:', this.state.year);
-    // console.log('color:', this.state.color);
+    // console.log('name:', state.name);
+    // console.log('year:', state.year);
+    // console.log('color:', state.color);
   };
-  
-  render() {
-    const year = new Date().getFullYear();
 
-    return (
-      <div>
-        <h1>{this.state.titre}</h1>
+  const year = new Date().getFullYear();
 
-        <button onClick={this.addTenYears}>+ 10 ans</button>
+  return (
+    <div>
+      <h1>{state.titre}</h1>
 
+      <button onClick={addTenYears}>+ 10 ans</button>
 
-        <form onSubmit={this.handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <label>
           name:
           <input
             type="text"
             name="name"
-            value={this.state.name}
-            onChange={this.handleChangeForm}
+            value={state.name}
+            onChange={handleChangeForm}
           />
         </label>
         <br />
@@ -106,8 +96,8 @@ class Mycars extends Component {
           <input
             type="text"
             name="age"
-            value={this.state.age}
-            onChange={this.handleChangeForm}
+            value={state.age}
+            onChange={handleChangeForm}
           />
         </label>
         <br />
@@ -116,34 +106,33 @@ class Mycars extends Component {
           <input
             type="text"
             name="color"
-            value={this.state.color}
-            onChange={this.handleChangeForm}
+            value={state.color}
+            onChange={handleChangeForm}
           />
         </label>
         <br />
         <button type="submit">Valider</button>
       </form>
 
+      <input
+        type="text"
+        placeholder="entrer 1 2 ou 3"
+        onChange={handleChange}
+      />
+      <button onClick={DeleteSpecificCar}>Supprimer une voiture</button>
 
-        <input type="text" placeholder="entrer 1 2 ou 3"  onChange={this.handleChange}/>
-        <button onClick={this.DeleteSpecificCar}>Supprimer une voiture</button>
-        
-        {this.state.voitures.map((voitures, index) => {
-          return (
-            <div key={index}>
-              <Car color={voitures.color} year={year - voitures.year + " ans"}>
-                {voitures.name}
-              </Car>
-            </div>
-          );
-        })}
-        {/* <Car color={this.state.voitures[0].color} year={year - this.state.voitures[0].year +'ans'} >{this.state.voitures[0].name}</Car>
-                <Car color={this.state.voitures[1].color} year={year - this.state.voitures[1].year +'ans'}>{this.state.voitures[1].name}</Car>
-                <Car color={this.state.voitures[2].color} year={year - this.state.voitures[2].year +'ans'}>{this.state.voitures[2].name}</Car> */}
-      </div>
-    );
-  }
+      {state.voitures.map((voitures, index) => {
+        return (
+          <div key={index}>
+            <Car color={voitures.color} year={year - voitures.year + " ans"}>
+              {voitures.name}
+            </Car>
+          </div>
+        );
+      })}
+      {/* <Car color={state.voitures[0].color} year={year - state.voitures[0].year +'ans'} >{state.voitures[0].name}</Car>
+                <Car color={state.voitures[1].color} year={year - state.voitures[1].year +'ans'}>{state.voitures[1].name}</Car>
+                <Car color={state.voitures[2].color} year={year - state.voitures[2].year +'ans'}>{state.voitures[2].name}</Car> */}
+    </div>
+  );
 }
-
-
-export default Mycars;
